452d7d81d771426c1be08d3403bdffaac1a34811
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-08-24 16:09:11 +0300

    fix deployment

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,30 +1,29 @@
-
-const {Pool}= require('pg')
-
+const { Pool } = require('pg');
 require('env2')('.env');
-const url =  process.env.DEV_DB_URL
-if (!url) throw new Error('Environment variable DB_URL must be set');
 
-// const {NODE_ENV, DB_URL, DEV_DB_URL, TEST_DB_URL} = process.env;
-// let dbUrl = "";
+let dbUrl = '';
+if (!dbUrl) throw new Error('Environment variable DB_URL must be set');
 
-// switch(NODE_ENV) {
-//     case 'production':
-//         dbUrl = DB_URL;
-//     break;
-//     case 'development':
-//         dbUrl = DEV_DB_URL;
-//     break;
-//     case 'test':
-//         dbUrl = TEST_DB_URL;
-//     break;
-//     default:
-//         throw new Error('No Database is found !');
-// }
-const connection = new Pool ({
-    connectionString :  url,//dbUrl,
-    // ssl: {         rejectUnauthorized: false       }
-    ssl: false,
-}) 
+const {
+  NODE_ENV, DATABASE_URL, DEV_DB_URL, TEST_DB_URL,
+} = process.env;
+
+switch (NODE_ENV) {
+  case 'production':
+    dbUrl = DATABASE_URL;
+    break;
+  case 'development':
+    dbUrl = DEV_DB_URL;
+    break;
+  case 'test':
+    dbUrl = TEST_DB_URL;
+    break;
+  default:
+    throw new Error('No Database is found !');
+}
+const connection = new Pool({
+  connectionString: dbUrl,
+  ssl: NODE_ENV !== 'production' ? false : { rejectUnauthorized: false },
+});
 
 module.exports = connection;
