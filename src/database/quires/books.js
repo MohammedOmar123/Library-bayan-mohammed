38f678665ca24d0f1d7a7991709666e317719902
bayan2002 <bayanabdelbaryq@gmail.com>
2022-08-24 11:35:41 +0300

    Queries for books and categories function to handle data that get back from the database function to handle data in the front side releates #12

---
+++ src/database/quires/books.js
@@ -0,0 +1,13 @@
+const connection = require('../connection')
+
+const getAllBooks = () => connection.query(' SELECT books.id , books.bName , books.img , categories.cName  from books join categories on(books.category_id= categories.id);');
+
+
+const postBook =(name ,imgUrl , id) =>
+     connection.query({
+        text: 'INSERT INTO books (bName, img, category_id) VALUES ($1, $2, $3);',
+        values: [name, imgUrl, id],
+      });
+
+
+module.exports ={ getAllBooks , postBook }
